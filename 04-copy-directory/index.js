51d2df6baf345cbead379396820e1265e67fbcf5
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-05-19 14:26:26 +0500

    feat: finish fourth part

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const path = require('path');
+const { mkdir, readdir, copyFile, rm } = require('fs/promises');
 
+async function copyDir(from, to) {
+  async function delayedFileCopy(file) {
+    const srcPath = path.join(from, file.name);
+    const destPath = path.join(to, file.name);
+
+    if (file.isFile()) {
+      await copyFile(srcPath, destPath);
+    } else {
+      await copyDir(srcPath, destPath);
+    }
+  }
+
+  try {
+    await rm(to, { force: true, recursive: true });
+    await mkdir(to, { recursive: true });
+
+    const folderContent = await readdir(from, { withFileTypes: true });
+
+    const promises = folderContent.map(delayedFileCopy);
+    await Promise.all(promises);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const inputDirPath = path.join(__dirname, 'files');
+const outputDirPath = path.join(__dirname, 'files-copy');
+
+copyDir(inputDirPath, outputDirPath);
